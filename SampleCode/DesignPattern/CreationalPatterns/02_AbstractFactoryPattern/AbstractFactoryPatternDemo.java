7ce1bdc6b3f6736fc3ee3a861cdc8570655e4056
Ray Kuo <ray20200720@gmail.com>
2022-03-11 17:12:46 +0800

    Create CreationalPatterns Folder and Add 02_AbstractFactoryPattern'

---
+++ SampleCode/DesignPattern/CreationalPatterns/02_AbstractFactoryPattern/AbstractFactoryPatternDemo.java
@@ -0,0 +1,12 @@
+public class AbstractFactoryPatternDemo {
+    public static void main(String[] args) {
+        AbstractFactory shapeFactory = FactoryProducer.getFactory("SHAPE");
+        Shape shape1 = shapeFactory.getShape("CIRCLE");
+        shape1.draw();
+        
+        AbstractFactory colorFactory = FactoryProducer.getFactory("COLOR");
+        Color color1 = colorFactory.getColor("RED");
+        color1.fill();
+    }
+    
+}
